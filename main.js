e76d114347c51e1f273a760c8f31b4e3f5f0b79c
1258228272@qq.com <kingbluezxf1125>
2022-12-21 17:03:23 +0800

    项目基本页面框架搭建，引入uView插件

--- main.js
+++ main.js
@@ -5,17 +5,22 @@ import Vue from 'vue'
 Vue.config.productionTip = false
 App.mpType = 'app'
 const app = new Vue({
-    ...App
+	...App
 })
 app.$mount()
 // #endif
 
 // #ifdef VUE3
-import { createSSRApp } from 'vue'
+import {
+	createSSRApp
+} from 'vue'
 export function createApp() {
-  const app = createSSRApp(App)
-  return {
-    app
-  }
+	const app = createSSRApp(App)
+	return {
+		app
+	}
 }
 // #endif
+
+import uView from '@/uni_modules/uview-ui'
+Vue.use(uView)
---
+++ pages/center/center.vue
@@ -0,0 +1,14 @@
+<template>
+	<view>个人中心</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	},
+	methods: {}
+};
+</script>
+
+<style></style>
